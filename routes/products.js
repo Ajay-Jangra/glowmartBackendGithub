2281717043b775d1cbd1377b32c57832f1b1750c
ajay-jangra <ajayjangrarakhi@gmail.com>
2024-12-28 12:59:12 +0530

    products updated

--- routes/products.js
+++ routes/products.js
@@ -25,6 +25,32 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: 'Error adding product', error });
     }
 });
+// Add new product
+router.post('/', async (req, res) => {
+    const { id,title, disc, image, price, discount,rating  } = req.body;
+    try {
+        const newProduct = new Product({ id, title, disc, image, price, discount, rating });
+        await newProduct.save();
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to create product', error: error.message });
+    }
+});
+
+// Update product
+router.put('/:id', async (req, res) => {
+    const { id, title, disc, image, price, discount, rating } = req.body;
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            { id, title, disc, image, price, discount, rating },
+            { new: true }
+        );
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update product', error: error.message });
+    }
+});
 
 module.exports = router;
 
